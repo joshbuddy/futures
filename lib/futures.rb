6cd19f4f91a5cf0c81dfb82f0bb01cee1760196c
Joshua Hull <joshbuddy@gmail.com>
2009-11-27 04:00:48 -0500

    Version bump to 0.0.0

---
+++ lib/futures.rb
@@ -0,0 +1,50 @@
+module Futures
+
+  class Value
+
+    TimeoutError = Class.new(RuntimeError)
+
+    def read(timeout = nil)
+      unless @value
+        @write = Thread.new { Thread.stop }
+        @write.join(timeout) or raise(TimeoutError.new)
+      end
+      @value
+    end
+
+    def write(value)
+      @value = value
+      @write.wakeup if @write
+    end
+  end
+  
+  def self.included(cls)
+    cls.class_eval "
+      class << self
+        include Futures::ClassMethods
+      end
+    "
+  end
+
+  module ClassMethods
+    def future(*attrs)
+      attrs.each do |attr|
+        self.class_eval "
+          def #{attr}(timeout = nil)
+            @__#{attr}_future ||= Futures::Value.new
+            @__#{attr}_future.read(timeout)
+          end
+
+          def #{attr}=(val)
+            @__#{attr}_future ||= Futures::Value.new
+            @__#{attr}_future.write(val)
+          end
+
+        "
+      end
+    end
+  end
+  
+end
+
+
